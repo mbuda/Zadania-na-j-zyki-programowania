485d8dd70bc764827c9225cdb542a19d26b40b56
Marta Buda <bmarta1991@o2.pl>
2012-05-18 15:43:26 +0200

    zmiany

--- JP1.5-2.c
+++ JP1.5-2.c
@@ -9,11 +9,13 @@ int main()
    scanf("%i", &licz1);
    printf("Podaj druga liczbe: ");
    scanf("%i", &licz2);
-   if(licz1=licz2)	
-		printf("Liczby %i i %i sa rowne.");
+   if(licz1==licz2)	
+	printf("Liczby %i i %i sa rowne.", licz1, licz2);
+else {
    if (licz1>licz2)
      printf("Wieksza jest liczba %i", licz1);
 	else 
 		printf("Wieksza jest liczba %i", licz2);
+}
    return 0;
 }
